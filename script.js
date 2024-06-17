6343756445feb2097359ab475a2f73f7bc80a25b
Malai Durai <158719877+RMDURAI@users.noreply.github.com>
2024-06-17 20:10:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+ const tabs=document.querySelectorAll('.tab-btn');
+ const all_content =document.querySelectorAll('.content');
+
+ tabs.forEach((tab,index)=>{
+tab.addEventListener('click',(e)=>{
+tabs.forEach(tab=>{tab.classList.remove('active')});
+tab.classList.add('active');
+
+var line=document.querySelector('.line');
+line.style.width = e.target.offsetWidth + "px";
+line.style.left = e.target.offsetLeft + "px";
+
+all_content.forEach(content=>{content.classList.remove('active')});
+all_content[index].classList.add('active');
+});
+});
+ 
+ function Homeclick(Cnt){
+    // alert(Cnt)
+    if(Cnt==1){
+        $("#homecontent").show();
+        $("#Productcnt").hide();
+        $("#aboutcnt").hide();
+        $("#contactcnt").hide();
+    }
+    else if(Cnt==2){
+        $("#Productcnt").show();
+        $("#homecontent").hide();
+        $("#aboutcnt").hide();
+        $("#contactcnt").hide();
+    }
+    else if(Cnt==3){
+        
+        $("#homecontent").hide();
+        $("#Productcnt").hide();
+        $("#aboutcnt").show();
+        $("#contactcnt").hide();
+    }
+    else if(Cnt==4){
+        $("#contactcnt").show();
+        $("#homecontent").hide();
+        $("#Productcnt").hide();
+        $("#aboutcnt").hide();
+    }
+ }
